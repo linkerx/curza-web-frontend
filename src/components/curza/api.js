427c294ca7047a5c68d87e2b37fbedee81e6c709
linkerx <diego.martinezdiaz@linkerx.com.ar>
2019-08-27 18:15:39 -0300

    correcion rutas api curza

--- src/components/curza/api.js
+++ src/components/curza/api.js
@@ -3,16 +3,18 @@ var axios = require('axios');
 /**
  * Api Variables
  */
+var WpUrl = process.env.REACT_APP_WP_API_URL;
+var WpApiDir = process.env.REACT_APP_WP_API_PREFIX;
 var ProgramasUrl = process.env.REACT_APP_PROGRAMAS_URL;
 var ProgramasPrefix = process.env.REACT_APP_PROGRAMAS_PREFIX;
 var ProgramasRoute = process.env.REACT_APP_PROGRAMAS_ROUTE;
 
-//var CurzaRoute = '/curza/v1';
-//var LnkSitesEndpoint = '/sites';
+var CurzaRoute = '/curza/v1';
+var LnkSitesEndpoint = '/sites';
 
 // Subsitio Curza
 export function getSite(options){
-    var url = ProgramasUrl +'/'+ ProgramasPrefix +'/'+ ProgramasRoute+ '/' + options.name;
+    var url = WpUrl +'/'+ WpApiDir + CurzaRoute + LnkSitesEndpoint + '/' + options.name;
     if(options.debug){
         console.log(options,url);
     }
