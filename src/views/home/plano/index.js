93824fbc94efd8b8aecd15147e12054c4e53b908
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-06-22 00:40:08 -0300

    reactsvg

--- src/views/home/plano/index.js
+++ src/views/home/plano/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './styles.scss';
+import { ReactSVG } from 'react-svg'
 
 class HomePlano extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.test = this.test.bind(this)
+  }
+
+  test(svg){
+    const aulas = svg.getElementsByClassName('aula');
+    for (let i = 0; i < aulas.length; i++) {
+      aulas.item(i).addEventListener('click', (e) => {
+        console.log(e);
+      })
+    }
+  }
+
   render(){
     return(
-      <section id='home-proyectos'>
+      <section id='home-plano'>
         <div className='wrapper-central'>
           <h1>Plano Curza</h1>
+          <ReactSVG src='images/planoCurza.svg' afterInjection={this.test}/>
         </div>
       </section>  
     )
